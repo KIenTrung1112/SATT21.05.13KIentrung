a9f767d8c1680144db92b46d75cfc3ce6006ccef
SATT21.05.13KienTrung <trung6713@gmail.com>
2021-12-23 11:19:21 +0700

    update

--- src/main/java/Common/Utilities.java
+++ src/main/java/Common/Utilities.java
@@ -2,6 +2,6 @@ package Common;
 
 public class Utilities {
     public static String getProjectPath() {
-        return "C:\\user\\trung\\ideaProject\\SATT21.05.13KienTrung";
+        return "C:\\user\\trung\\ideaProject\\SATT21.05.13KienTrung\\Executables\\chromedriver.exe";
     }
 }
